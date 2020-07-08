f8432d7a7539da4b2b77c0a040f1860e8fa28e70
renataalmeids <ras_almeida@hotmail.com>
2020-07-08 14:02:55 -0300

    iniciando a função mdlinks

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+
+const fs = require('fs');
+path = "C:\\Users\\Samuel\\Documents\\Laboratoria\\SAP004-md-links"
+fs.readdir(path, 'utf8', (err, data) => {
+  if(err){
+    console.log (err)
+  } else { //array  de obj
+    data.forEach(file => {
+      console.log(file)
+      console.log(data)
+    //const oi = data.match(/http[s]?:/)
+    //console.log (oi)
+  })
+}});
+/*
+fs.stat(path, (err, stats) => { 
+    if( !err ){ 
+        if(stats.isFile()){ 
+            console.log('is file ? ' + stats.isFile()); 
+            fs.readFile(path, 'utf8', (err, data) => {
+              if(err){
+                console.log (err)
+              } else {
+                console.log ("O conteudo da file contained:", data)
+              }
+            });
+            
+        } 
+  
+        else if(stats.isDirectory()){ 
+            console.log('is directory? ' + stats.isDirectory());
+            fs.readdir(path, 'utf8', (err, data) => {
+              if(err){
+                console.log (err)
+              } else {
+                console.log ("O conteudo da file contained:", data)
+              }
+            }); 
+        } 
+    } 
+    else
+        throw err;  
+});
+*/
---
+++ teste/teste.md
@@ -0,0 +1,2 @@
+Que o próximo projeto seja mais fácil
+[`course-parser`](https://github.com/Laboratoria/course-parser)
