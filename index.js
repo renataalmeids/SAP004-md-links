40e447f674cda3f93e66ec1687c2fc1bfe8d0c5b
renataalmeids <ras_almeida@hotmail.com>
2020-07-09 15:01:35 -0300

    lendo o caminho

--- index.js
+++ index.js
@@ -3,45 +3,46 @@
 };*/
 
 const fs = require('fs');
-path = "C:\\Users\\Samuel\\Documents\\Laboratoria\\SAP004-md-links"
-fs.readdir(path, 'utf8', (err, data) => {
-  if(err){
-    console.log (err)
-  } else { //array  de obj
-    data.forEach(file => {
-      console.log(file)
-      console.log(data)
-    //const oi = data.match(/http[s]?:/)
-    //console.log (oi)
-  })
-}});
-/*
-fs.stat(path, (err, stats) => { 
-    if( !err ){ 
-        if(stats.isFile()){ 
-            console.log('is file ? ' + stats.isFile()); 
-            fs.readFile(path, 'utf8', (err, data) => {
-              if(err){
-                console.log (err)
-              } else {
-                console.log ("O conteudo da file contained:", data)
-              }
-            });
-            
-        } 
-  
-        else if(stats.isDirectory()){ 
-            console.log('is directory? ' + stats.isDirectory());
-            fs.readdir(path, 'utf8', (err, data) => {
-              if(err){
-                console.log (err)
-              } else {
-                console.log ("O conteudo da file contained:", data)
-              }
-            }); 
-        } 
-    } 
-    else
-        throw err;  
-});
-*/
+const { argv } = require('process');
+// path = "C:\\Users\\Samuel\\Documents\\Laboratoria\\SAP004-md-links"
+// fs.readdir(path, 'utf8', (err, data) => {               /*lendo o arquivo md*/
+//   if(err){
+//     console.log (err)
+//   } else { //array  de obj
+//     data.forEach(file => {
+//       console.log(file)
+//       console.log(data)
+//     //const oi = data.match(/http[s]?:/)
+//     //console.log (oi)
+//   })
+// }});
+
+function cheking (path) {
+
+  fs.stat(path, (err, stats) => { 
+          if(stats.isFile()){ 
+              console.log('is file ? ' + stats.isFile()); 
+              fs.readFile(path, 'utf8', (err, data) => {
+                if(err){
+                  console.log (err)
+                } else {
+                  console.log ("O conteudo da file contained:", data)
+                }
+              });
+              
+          } 
+    
+          else if(stats.isDirectory()){ 
+              console.log('is directory? ' + stats.isDirectory());
+              fs.readdir(path, 'utf8', (err, data) => {
+                if(err){
+                  console.log (err)
+                } else {
+                  console.log ("O conteudo da file contained:", data)
+                }
+              }); 
+          } 
+  });
+}
+cheking (process.argv[2])
+
