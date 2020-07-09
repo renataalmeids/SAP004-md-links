4672f12188131890f0f952292b5c070528675da8
renataalmeids <ras_almeida@hotmail.com>
2020-07-09 15:25:17 -0300

    Separando em objeto

--- index.js
+++ index.js
@@ -17,32 +17,34 @@ const { argv } = require('process');
 //   })
 // }});
 
-function cheking (path) {
+function cheking(path) {
 
-  fs.stat(path, (err, stats) => { 
-          if(stats.isFile()){ 
-              console.log('is file ? ' + stats.isFile()); 
-              fs.readFile(path, 'utf8', (err, data) => {
-                if(err){
-                  console.log (err)
-                } else {
-                  console.log ("O conteudo da file contained:", data)
-                }
-              });
-              
-          } 
-    
-          else if(stats.isDirectory()){ 
-              console.log('is directory? ' + stats.isDirectory());
-              fs.readdir(path, 'utf8', (err, data) => {
-                if(err){
-                  console.log (err)
-                } else {
-                  console.log ("O conteudo da file contained:", data)
-                }
-              }); 
-          } 
+  fs.stat(path, (err, stats) => {
+    if (stats.isFile()) {
+      fs.readFile(path, 'utf8', (err, data) => {
+        const regex = data.match(/\[(.[^\]]*)\]\((http.*)\)/gm)
+        const linkRegex = regex.map(e => {
+          const result = e.match(/\[(.[^\]]*)\]\((http.*)\)/)
+          return{
+            text: result[1],
+            href:result [2]
+          }
+        })
+        console.log(linkRegex)
+      })
+    }
+
+    else if (stats.isDirectory()) {
+      console.log('is directory? ' + stats.isDirectory());
+      fs.readdir(path, 'utf8', (err, data) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("O conteudo da file contained:", data)
+        }
+      });
+    }
   });
 }
-cheking (process.argv[2])
+cheking(process.argv[2])
 
--- teste/teste.md
+++ teste/teste.md
@@ -1,2 +1,4 @@
 Que o próximo projeto seja mais fácil
 [`course-parser`](https://github.com/Laboratoria/course-parser)
+Que o próximo projeto seja mais fácil
+[`course-parser`](https://github.com/Laboratoria/course-parser)
