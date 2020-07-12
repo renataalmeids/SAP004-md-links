4430ba7a4068d9b497135adb59b7d5a34f0872f0
renataalmeids <ras_almeida@hotmail.com>
2020-07-12 20:29:47 -0300

    Tentativa de novas implentações e teste

--- cli.js
+++ cli.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 const mdLinks = require ('./index.js')
 const files = process.argv[2];
+
+mdLinks(files)
+.then (result => {
+    console.log(result)
+}).catch((err) => {
+    console.log(err)
+});
+
--- index.js
+++ index.js
@@ -2,39 +2,40 @@
   // ...
 };*/
 
+
 const fs = require('fs');
 const { argv } = require('process');
 
-function checkIn(path) {
-
-  fs.stat(path, (err, stats) => {
-    if (stats.isFile()) {
-      fs.readFile(path, 'utf8', (err, data) => {
-        const regex = data.match(/\[(.[^\]]*)\]\((http.*)\)/gm)
-        const linkRegex = regex.map(e => {
-          const result = e.match(/\[(.[^\]]*)\]\((http.*)\)/)
-          return{
-            text: result[1],
-            href:result [2]
-          }
+function mdLinks(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, stats) => {
+      if (stats.isFile()) {
+        fs.readFile(path, 'utf8', (err, data) => {
+          const regex = data.match(/\[(.[^\]]*)\]\((http.*)\)/gm)
+          const linkRegex = regex.map(e => {
+            const result = e.match(/\[(.[^\]]*)\]\((http.*)\)/)
+            return console.log({
+              text: result[1],
+              href: result[2]
+            })
+          })
+          resolve(linkRegex)
         })
-        console.log(linkRegex)
-      })
-    }
+      }
 
-    else if (stats.isDirectory()) {
-      console.log('is directory? ' + stats.isDirectory());
-      fs.readdir(path, 'utf8', (err, data) => {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log("O conteudo da file contained:", data)
-        }
-      });
-    }
-  });
+      else if (stats.isDirectory()) {
+        console.log('is directory? ' + stats.isDirectory());
+        fs.readdir(path, 'utf8', (err, data) => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log("O conteudo da file contained:", data)
+          }
+        });
+      }
+    });
+  })
 }
-checkIn(process.argv[2]);
+mdLinks(process.argv[2]);
 
 module.exports = mdLinks;
-
